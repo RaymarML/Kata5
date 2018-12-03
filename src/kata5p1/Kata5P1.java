2f887f37ed3bd6ff0ee0e67c83ce1be0ad121909
RaymarML <32089732+RaymarML@users.noreply.github.com>
2018-12-03 16:19:35 +0000

    Kata5 parte1 version3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,32 +1,43 @@
 package kata5p1;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class Kata5P1 {
 
+    private final static String url = "jdbc:sqlite:Kata5.db";
+    private final static String path = "D:\\RAYMAR\\universidad\\3-año\\IS2\\Kata5\\Kata5P1\\emails.txt";
     
-    
-    public static void main(String[] args) throws SQLException {
-        createNewTable("jdbc:sqlite:Kata5.db", "EMAIL");
-    }   
-      
- 
-    public static void createNewTable(String url, String table) {
-        // Instrucción SQL para crear nueva tabla
-        String sql= "CREATE TABLE IF NOT EXISTS " + table + " (\n"
-            + "id integer PRIMARY KEY AUTOINCREMENT,\n"
-            + "mail text NOT NULL);";
-        try (Connection conn = DriverManager.getConnection(url);
-                Statement stmt = conn.createStatement()) {
-            // Se crea la nueva tabla
-            stmt.execute(sql);
-            System.out.println("Tabla creada");
+    public static void main(String[] args) throws SQLException, IOException {
+        String table = "EMAIL";
+        List<String> listMails = new MailListReader().read(path);
+        String sql = "INSERT INTO " + table + "(mail)" + " VALUES(?)";
+        
+        try (Connection conn= connect();
+            PreparedStatement pstmt= conn.prepareStatement(sql)) {
+            for (String email : listMails) {
+                pstmt.setString(1, email);
+                pstmt.executeUpdate();
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }  
+        
+    public static Connection connect() {
+    // Cadena de conexión SQLite
+        Connection conn= null;
+        try {
+            conn= DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
 }
